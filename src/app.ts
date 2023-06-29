b6b826310f594ae78174a0e0f37459b965584260
Daniel Pineda <angeldpb27@gmail.com>
2023-06-28 20:30:19 -0500

    [+] app.ts: define 2 endpoints, /, /home

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,25 @@ import express from 'express';
 
 const app = express ();
 
+/** Routing: enrutamiento */
+/** http://localhost:3000/ */
+app.get ('/', (req, res) => {   /**req = request, res = response */
+    const message = 'Bienvenido a la API de mudar de colombia'
+    console.log(message);   //mensaje al navegador
+
+    res.send (message);     //mensaje a la terminal
+    res.send (`<h1> ${message}</h1>`);
+});
+
+/** http://localhost:3000/home (Endpoint) */
+app.get('/home', (req, res) => {
+  const namePage = 'Home';
+
+  console.log(namePage);
+  res.send (`<h1> ${namePage}</h1>`);
+})
+
+
 app.listen(3000, () => {
   console.log('Servidor lanzado en el puerto 3000');
 });

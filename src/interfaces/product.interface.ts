fb639c31e5543bc9a5d4716f4b9add8e5849bd46
Daniel Pineda <angeldpb27@gmail.com>
2023-06-30 20:28:29 -0500

    [+] product interface: Define estructura de la interfaz

---
+++ src/interfaces/product.interface.ts
@@ -0,0 +1,6 @@
+// Interface: unicamente se modela y define la estructura de datos y sus tipos, no se agregan funcionalidades o metodos //
+export interface product {
+    name: string;
+    price: number;
+    description: string;
+}

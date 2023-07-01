572a41afa838a21eacc1f236688004282df40eb6
Daniel Pineda <angeldpb27@gmail.com>
2023-06-30 21:37:41 -0500

    [+] mongo.config: agregamos la URI de Mongo el nombre de la base de datos

--- src/config/mongo.config.ts
+++ src/config/mongo.config.ts
@@ -3,7 +3,7 @@ import { connect } from "mongoose";
 
 /** Configuración para la conexión con MongoDB */
 async function dbConnect () : Promise<void>   {
-    const DB_URI = 'mongodb://127.0.0.1:27017';
+    const DB_URI = 'mongodb://127.0.0.1:27017/mudar';
     await connect(DB_URI);
 }
 
